c91edfce8e2175d42629282878f969a0aecbbf32
aivudm <aivudm@rambler.ru>
2026-04-16 18:28:52 +0300

    Размещение

---
+++ Яндекс.Крауд/Test_Yandex_Task1/Test_Yandex_Task1.cpp
@@ -0,0 +1,130 @@
+﻿#include <iostream>
+#include <sstream>
+//#include <string>
+#include <map>
+
+
+std::string sInputMessage("Input:\n"); //--- Вход:
+std::string sOutputMessage("Output:\n"); //--- Выход:
+std::string sErrorInputData("Error input data\n"); //--- Ошибка входных данных
+std::string sEOL = "\r\n";
+std::string sEONum = " ";
+
+
+//--- Основная функция расчитывающая остаток воды в увлажнителе
+//--- На входе: "многострочная строка" с количеством итераций по доливу воды
+std::string CalculateHumidifierWaterRest(std::string& inputData) {
+   
+    unsigned short  iterCount = 0, iT = 0, iV = 0, iResult = 0;
+    std::string sTmpBuffer = "", sTmpBuffer1 = "", outResult = "";
+    std::map <unsigned int, unsigned int> mapAddWater = {};    //--- контейнер для хранения пар числовых значений (T_i и V_i) операций по доливу воды
+//--- Загружаем входную строку в стринг стрим для последующего парсинга
+    std::stringstream ssBuffer(inputData);
+
+//--- Считываем первое значение из потока - количество итераций доливов
+    ssBuffer >> iterCount;
+    do 
+    {
+        ssBuffer >> iT >> iV;
+        mapAddWater[iT] = iV;
+    } while (!ssBuffer.eof());
+
+//--- Последовательный проход по итерациям долива/утечек
+//--- За количество проходов взято последнее (максимальное) значение T_i из входной строки
+    for (unsigned short i = 1; i <= iT; i++)
+    {
+//--- Если вода в увлажнителе есть, то вычтем утечку (= 1л) на текущей итерации
+        if (iResult > 0)
+        {
+            iResult--;
+        }
+
+//--- Проверка - есть ли доливка в увлажнитель на текущей итерации
+        if (mapAddWater.find(i) != mapAddWater.end())
+        {
+            iResult += mapAddWater[i];
+        }
+    }
+
+//--- Преобразуем результат в строку (согласно условию задачи)
+    outResult = std::to_string(iResult);
+    return outResult;
+}
+
+
+int main()
+{
+    std::string inputString = "";
+    unsigned short  iterCount = 0, iterT = 0, iterV = 0;
+    try
+    {
+        std::cout << sInputMessage;
+//--- Ввод и проверка корректности ввода количества циклов доливки воды
+//--- 
+        while (true)
+        {
+            if (std::cin >> iterCount) {
+               break;  //--- Как только считали корректное число итераций, следуем на продолжение считывания ланных
+            }
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            std::cout << sErrorInputData;
+        };
+
+//--- Запись кол-ва доливов (итераций) в итоговую строку с данными
+        inputString += std::to_string(iterCount);
+        inputString += sEOL; //--- Добавляем "\r\n" к текущей подстроке в составе итоговой строки
+
+//        std::cout << "Input pairs T_i, V_i\n";
+
+//--- Ввод и проверка корректности ввода количества циклов доливки воды
+        unsigned short iterT_pred = 0;
+        for (uint8_t i = 0; i < iterCount; i++)
+        {
+            while (true)
+            {
+                if (std::cin >> iterT >> iterV) {
+                   //--- Как только считали корректную пару T_i, V_i, записываем в итоговую строку и следуем на продолжение считывания ланных
+                    if (iterT > iterT_pred) //--- Дополнительная проверка на корректность ввода значения T_i (строго больше предыдущего)
+                    {
+                        iterT_pred = iterT; //--- Запоминаем последнее значение T_i
+
+                        inputString += std::to_string(iterT);
+                        inputString += sEONum;
+                        inputString += std::to_string(iterV);
+                        inputString += sEOL; //--- Добавляем "\r\n" к текущей подстроке в составе итоговой строки
+
+                        break;
+                    }
+                }
+                else
+                {
+                    std::cin.clear();
+                    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                    std::cout << sErrorInputData;
+                }
+            }
+
+        }
+    }
+    catch (...)
+    {
+        std::cout << sErrorInputData;
+        return 0;
+    }
+
+
+    std::cout << sOutputMessage << (CalculateHumidifierWaterRest(inputString));
+
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
---
+++ Яндекс.Крауд/Test_Yandex_Task2/Test_Yandex_Task2.cpp
@@ -0,0 +1,174 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+
+std::string sInputMessage1("Input Number A:\n");
+std::string sInputMessage2("Input Number B:\n");
+std::string sOutputMessage1("Output: maximized number A by digits from B: \n"); //--- Выход:
+std::string sOutputMessage2("Maximization number A by digits from B is not avaible. \n"); //--- Выход:
+std::string sErrorInputData("Error input data\n"); //--- Ошибка входных данных
+
+//--- Основная функция максимизирующая число А цифрами из числа В
+//--- На входе: число А (&sNumberA) и число В (&sNumberB)
+bool MaximizeAFromB(std::string &sNumberA, std::string &sNumberB)
+{
+    std::string outputResult = "";
+
+//--- Загрузка цифр числа B в контейнер для сортировки и уникализации цифр
+    std::vector<char> vDigitsB = {};
+    for (char& cDigit : sNumberB)
+    {
+        vDigitsB.push_back(cDigit);
+    }
+
+//--- Сортировка и уникализация цифр числа B
+//        std::ranges::sort(vDigitsA); //--- для компилятора С++20
+    std::sort(vDigitsB.begin(), vDigitsB.end());
+    auto itDigitsB = std::unique(vDigitsB.begin(), vDigitsB.end());
+    vDigitsB.erase(itDigitsB, vDigitsB.end());
+
+//--- Загрузка цифр числа A в контейнер
+    std::vector<char> vDigitsA = {};
+    for (char& cDigit : sNumberA)
+    {
+        vDigitsA.push_back(cDigit);
+    }
+
+ //--- Перебор цифр числа A от старших разрядов к младшим
+//--- Замена цифрами числа В при условии, что они больше
+
+
+    bool bIsChanged = false;
+    char cDigitB = {};
+
+    std::vector<char>::iterator itDigitsA = vDigitsA.begin();   //--- Для перебора цифр числа А слева направо 
+
+//--- Цикл прохода по всем цифрам числа А
+    do
+    {
+//--- Выбираем максимальную цифру числа В и удаляем её из контейнера        
+        cDigitB = vDigitsB.back();
+        vDigitsB.pop_back();
+
+//--- Сравнение текущей цифры числа В с цифрами числа А (от старших разрядов к младшим)
+//--- если цифрв числа А больше или равна, то пропуск (переход к следующей)
+        while ((*itDigitsA >= cDigitB) && (++itDigitsA != vDigitsA.end()))
+        {
+//            itDigitsA++;
+        }
+
+        if (itDigitsA != vDigitsA.end())
+        {
+//            *itDigitsA = cDigitB; //--- Замена только первой слева цифры числа А на цифру числа В
+//--- Поиск и замена всех данных цифр (если их несколько) 
+            std::vector<char>::iterator itCurDigitsA = itDigitsA;   //--- Для перебора цифр от текущего числа А слева направо 
+            auto cDigitA = *itDigitsA;
+            itCurDigitsA = std::find(itDigitsA, vDigitsA.end(), cDigitA);
+            while (itCurDigitsA != vDigitsA.end())
+            {
+                *itCurDigitsA = cDigitB;
+                itCurDigitsA = std::find(itCurDigitsA, vDigitsA.end(), cDigitA);
+            }
+            bIsChanged = true;
+        }
+
+    } while ((itDigitsA != vDigitsA.end()) and (!vDigitsB.empty()));
+
+
+//--- Если ни одна цифра числа А не была заменена то функция должна вернуть false
+    if (!bIsChanged)
+    {
+        return false;
+    }
+
+//--- Выгрузка цифр из контейнера в число A 
+    vDigitsA.begin();
+    sNumberA = "";
+
+    for (char& cDigit : vDigitsA)
+    {
+        sNumberA += cDigit;
+    }
+    return true;
+}
+
+static bool GetInputNumberAsString(std::string &sResultNumber)
+{
+//--- Ввод и проверка корректности ввода
+    try
+    {
+        while (true)
+        {
+            bool bIsStringNumber = true;
+            if (std::cin >> sResultNumber) {
+                for (char& cBuffer : sResultNumber)
+                {
+                    if (!isdigit(cBuffer))
+                    {
+                        bIsStringNumber = false;
+                        std::cout << sErrorInputData;
+                        std::cin.clear();
+                        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                        break;
+                    }
+                }
+            }
+            if (bIsStringNumber)
+            {
+                break; //--- Как только считали корректное число, выходим из цикла чтения
+            }
+
+        };
+
+    }
+    catch (...)
+    {
+        return false;
+    }
+
+    return true;
+}
+
+int main()
+{
+    std::string sNumberA = "", sNumberB = "";
+//--- Получение входных параметров 
+    try
+    {
+        std::cout << sInputMessage1;
+        while (!GetInputNumberAsString(sNumberA)) {}
+
+        std::cout << sInputMessage2;
+        while (!GetInputNumberAsString(sNumberB)) {}
+
+//--- Вызов функции, реализующей алгоритм и вывод результата
+        if (MaximizeAFromB(sNumberA, sNumberB))
+        {
+            std::cout << sOutputMessage1 << sNumberA;
+        }
+        else
+        {
+            std::cout << sOutputMessage2;
+        }
+    }
+    catch (...)
+    {
+        std::cout << sErrorInputData;
+        return 0;
+    }
+
+
+
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
